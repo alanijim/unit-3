8b92e36b256b8f92ec430fe115ca64ce65522229
sounny <sounny@gmail.com>
2023-12-07 14:27:21 -0500

    Fixing projections and imports.

--- js/test.js
+++ js/test.js
@@ -4,8 +4,8 @@ window.onload = setMap();
 // set up choropleth map
 function setMap() {
     // map frame dimensions
-    var width = 960,
-        height = 460;
+    var width = 700,
+        height = 400; //just setting to make it easier to preview
 
     // create new svg container for the map
     var map = d3.select("body")
@@ -14,32 +14,54 @@ function setMap() {
         .attr("width", width)
         .attr("height", height);
 
-    // create Albers equal area conic projection centered on France
-    var projection = d3.geoAlbers()
-        .center([0, 46.2])
-        .rotate([-2, 0, 0])
-        .parallels([43, 62])
-        .scale(2500)
-        .translate([width / 2, height / 2]);
+    // // create Albers equal area conic projection centered on France
+    // var projection = d3.geoAlbers()
+    //     .center([0, 46.2])
+    //     .rotate([-2, 0, 0])
+    //     .parallels([43, 62])
+    //     .scale(2500)
+    //     .translate([width / 2, height / 2]);
+
+    // Define projection
+    var projection = d3.geoRobinson()
+            .scale(100) // Adjust as needed
+            .translate([width / 2, height / 2]); // Center the map
+;
 
     var path = d3.geoPath()
         .projection(projection);
 
     // use Promise.all to parallelize asynchronous data loading
     var promises = [];    
-    promises.push(d3.csv("data/GeographyRegionsData.csv")); // load attributes from csv    
-    promises.push(d3.json("data/GeographyRegionz.topojson")); // load background spatial data    
+    promises.push(d3.csv("data/GeographyRegionsData.csv")); // load attributes from csv   
+    promises.push(d3.json("data/Land.topojson")); 
+    promises.push(d3.json("data/GeographyRegionz.topojson")); // load background spatial data  
+  
     
     Promise.all(promises).then(callback);
 
     // use Promise.all to parallelize asynchronous data loading
     function callback(data) {
         var csvData = data[0],
-            regionData = data[1];
+            land = data[1],
+            regionData = data[2];
+
+            console.log(land);
 
         var geographyRegions = topojson.feature(regionData, regionData.objects.GeographyRegionz);
+        var geoland = topojson.feature(land, land.objects.ne_50m_land);
 
-        // add France regions to map
+              // // add land to map
+              var land = map.selectAll(".land")
+              .data(geoland.features)
+              .enter()
+              .append("path")
+              .attr("class", "land")
+              .attr("d", path)
+              .style("fill", "green") // Add fill color
+              .style("stroke", "black"); // Add stroke color;
+
+        // add  regions to map
         var regions = map.selectAll(".regions")
             .data(geographyRegions.features)
             .enter()
@@ -47,9 +69,23 @@ function setMap() {
             .attr("class", function(d){
                 return "regions " + d.properties.NE_ID;
             })
-            .attr("d", path);
+            .attr("d", path)
+            .style("fill", "blue") // Add fill color
+            .style("stroke", "black"); // Add stroke color;
 
+  
+
+    
         console.log(csvData);
         console.log(regions);
+
+        
     }
+
+    // add graticles to the map
+    var graticles = map.append("path")
+        .datum(d3.geoGraticule())
+        .attr("class", "graticles")
+        .attr("d", path);
+
 }
